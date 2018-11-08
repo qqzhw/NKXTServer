21272c55bf921023158a50caef9da4969b35c459
qqzhw <qqzhw@DESKTOP-ZHW>
2018-11-09 00:15:00 +0800

    department

---
+++ Web/ICIMS.Application/BaseData/DepartmentAppService.cs
@@ -0,0 +1,17 @@
+﻿using Abp.Domain.Repositories;
+using Abp.Organizations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ICIMS.BaseData
+{
+   public class DepartmentAppService: OrganizationUnitManager,IDepartmentAppService
+    {
+        private IRepository<OrganizationUnit, long> _organizationUnitRepository;
+        public DepartmentAppService(IRepository<OrganizationUnit, long>  organizationUnitRepository):base(organizationUnitRepository)
+        {
+            _organizationUnitRepository = organizationUnitRepository;
+        }
+    }
+}
