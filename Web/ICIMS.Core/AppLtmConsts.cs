30b8a70f800a9b5830ff5528615fff2bc6c689ec
qqzhw <qqzhw@DESKTOP-ZHW>
2018-11-21 18:06:49 +0800

    分页大小

--- Web/ICIMS.Core/AppLtmConsts.cs
+++ Web/ICIMS.Core/AppLtmConsts.cs
@@ -6,7 +6,7 @@ namespace ICIMS
 	{
 		// TODO: 52ABP代码生成器默认生成的一些常量类 ，不影响业务逻辑点
 		public const int DefaultPageSize = 10;
-		public const int MaxPageSize = 1000;
+		public const int MaxPageSize = int.MaxValue;
 
 
 
